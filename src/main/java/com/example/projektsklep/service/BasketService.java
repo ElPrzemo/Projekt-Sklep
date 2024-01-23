36e243958417bdc50734bfa1b4d7492e0513dd13
Przemek <przemyslawwlodarczyk90@gmail.com>
2024-01-23 21:49:37 +0100

    Builded basket logic.

--- src/main/java/com/example/projektsklep/service/BasketService.java
+++ src/main/java/com/example/projektsklep/service/BasketService.java
@@ -1,11 +1,14 @@
 package com.example.projektsklep.service;
 
-
+import com.example.projektsklep.model.dto.LineOfOrderDTO;
+import com.example.projektsklep.model.dto.OrderDTO;
 import com.example.projektsklep.model.entities.product.Product;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.SessionScope;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -23,12 +26,37 @@ public class BasketService {
     }
 
     public Map<Long, Integer> getProducts() {
-        return products;
+        return new HashMap<>(products);
     }
 
     public void clear() {
         products.clear();
     }
 
+    public OrderDTO createInitialOrderDTO() {
+        // Tutaj tworzymy początkowe DTO, możemy ustawić domyślne wartości lub puste listy
+        return OrderDTO.builder()
+                .lineOfOrders(new ArrayList<>())
+                // Ustaw pozostałe pola, jeśli są wymagane
+                .build();
+    }
 
+    public OrderDTO createOrderDTOFromBasket(OrderDTO currentOrderDTO) {
+        List<LineOfOrderDTO> lineOfOrders = new ArrayList<>();
+        for (Map.Entry<Long, Integer> entry : products.entrySet()) {
+            // Tu tworzymy LineOfOrderDTO z każdego produktu w koszyku
+            lineOfOrders.add(new LineOfOrderDTO(null, entry.getKey(), entry.getValue(), null)); // Uzupełnij odpowiednimi danymi
+        }
+
+        // Tworzenie nowej instancji OrderDTO z aktualizowanymi danymi
+        return OrderDTO.builder()
+                .id(currentOrderDTO.id())
+                .userId(currentOrderDTO.userId())
+                .orderStatus(currentOrderDTO.orderStatus())
+                .dateCreated(currentOrderDTO.dateCreated())
+                .sentAt(currentOrderDTO.sentAt())
+                .totalPrice(currentOrderDTO.totalPrice())
+                .lineOfOrders(lineOfOrders)
+                .build();
+    }
 }
