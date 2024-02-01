224880e889b05c6695c9dccbb3858c117e0b4a1f
MaksymZakharov <zakharovmaksym27@gmail.com>
2024-02-01 21:32:46 +0100

    exeptiol list : UserController DataAccessException EmailAlreadyTakenException GlobalExceptionHandler InvalidCityException InvalidUserDataException UserAlreadyExistsException. WeatherServiceException WeatherServiceUnavailableException

--- src/main/java/com/example/projektsklep/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/projektsklep/exception/GlobalExceptionHandler.java
@@ -29,5 +29,40 @@ public class GlobalExceptionHandler {
         model.addAttribute("error", "Wystąpił błąd podczas aktualizacji adresu");
         return "update_profile_error";
     }
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public String handleUserAlreadyExists(Model model) {
+        model.addAttribute("error", "Użytkownik o tym adresie e-mail już istnieje");
+        return "user_register";
+    }
+
+    @ExceptionHandler(InvalidUserDataException.class)
+    public String handleInvalidUserData(Model model) {
+        model.addAttribute("error", "Błędne dane użytkownika");
+        return "user_edit";
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public String handleDataAccessException(Model model) {
+        model.addAttribute("dataAccessError", "Wystąpił błąd podczas dostępu do danych");
+        return "weather";
+    }
+    @ExceptionHandler(WeatherServiceException.class)
+    public String handleWeatherServiceError(Model model) {
+        model.addAttribute("error", "Wystąpił błąd podczas pobierania danych pogodowych");
+        return "weather_error";
+    }
+
+    @ExceptionHandler(InvalidCityException.class)
+    public String handleInvalidCityError(Model model) {
+        model.addAttribute("error", "Podane miasto jest nieprawidłowe");
+        return "invalid_city_error";
+    }
+
+    @ExceptionHandler(WeatherServiceUnavailableException.class)
+    public String handleWeatherServiceUnavailableError(Model model) {
+        model.addAttribute("error", "Serwis pogodowy jest niedostępny. Spróbuj ponownie później.");
+        return "weather_error";
+    }
+
 
 }
